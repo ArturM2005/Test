975b324ee9d4cd3e9eebbe4684ecfe3e63277570
ArturM2005 <arturmuhamediarov@gmail.com>
2025-02-25 17:17:54 +0300

    Add routes for methods

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/rates/{currency?}', 'RateController@rates')->name('rates');
+Route::post('/rates/{currency_from}?{currency_to}?{value}', 'RateController@convert')->name('convert');
