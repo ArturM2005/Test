153c897b4f6d4a0371709949987031b1e1f01748
ArturM2005 <arturmuhamediarov@gmail.com>
2025-02-25 17:21:25 +0300

    Created RateController

---
+++ app/Http/Controllers/RateController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function rates(?string $currency = null)
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function convert(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+}
